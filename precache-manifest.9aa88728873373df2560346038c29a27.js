b181996873e499e65a9f040d968de14134d939c7
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-23 17:04:07 +0900

    Updates

---
+++ precache-manifest.9aa88728873373df2560346038c29a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1636d620fa20c954ee08f2020c7404",
+    "url": "/cautious-palm-tree/index.html"
+  },
+  {
+    "revision": "d5073e23f5d1ead9703d",
+    "url": "/cautious-palm-tree/static/css/main.601e6465.chunk.css"
+  },
+  {
+    "revision": "a09c35e6ba4178c1beaa",
+    "url": "/cautious-palm-tree/static/js/2.43145208.chunk.js"
+  },
+  {
+    "revision": "d5073e23f5d1ead9703d",
+    "url": "/cautious-palm-tree/static/js/main.81bdf190.chunk.js"
+  },
+  {
+    "revision": "a5508722a29e6db592f4",
+    "url": "/cautious-palm-tree/static/js/runtime~main.aed0f52f.js"
+  }
+]);
